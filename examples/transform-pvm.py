3fbb948002f8dec654d9a88c5b8b2c6ab326f3c9
Nicolas Rougier <Nicolas.Rougier@inria.fr>
2014-07-28 17:36:25 +0200

    Added examples

---
+++ examples/transform-pvm.py
@@ -0,0 +1,90 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright (c) 2014, Nicolas P. Rougier. All Rights Reserved.
+# Distributed under the (new) BSD License.
+# -----------------------------------------------------------------------------
+import numpy as np
+import glumpy as gp
+import glumpy.gl as gl
+import glumpy.glm as glm
+from makecube import makecube
+from glumpy.transforms import PVMProjection, Position3D
+
+
+vertex = """
+uniform vec4 u_color;
+attribute vec3 position;
+attribute vec4 color;
+varying vec4 v_color;
+void main()
+{
+    v_color = u_color * color;
+    gl_Position = <transform>;
+}
+"""
+
+fragment = """
+varying vec4 v_color;
+void main()
+{
+    gl_FragColor = v_color;
+}
+"""
+
+window = gp.Window(width=1024, height=1024)
+
+@window.event
+def on_draw():
+    global phi, theta
+
+    gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+
+    # Filled cube
+    gl.glDisable(gl.GL_BLEND)
+    gl.glEnable(gl.GL_DEPTH_TEST)
+    gl.glEnable(gl.GL_POLYGON_OFFSET_FILL)
+    cube['u_color'] = 1, 1, 1, 1
+    cube.draw(gl.GL_TRIANGLES, faces)
+
+    # Outlined cube
+    gl.glDisable(gl.GL_POLYGON_OFFSET_FILL)
+    gl.glEnable(gl.GL_BLEND)
+    gl.glDepthMask(gl.GL_FALSE)
+    cube['u_color'] = 0, 0, 0, 1
+    cube.draw(gl.GL_LINES, outline)
+    gl.glDepthMask(gl.GL_TRUE)
+
+    # Make cube rotate
+    theta += 0.5 # degrees
+    phi += 0.5 # degrees
+    model = np.eye(4, dtype=np.float32)
+    glm.rotate(model, theta, 0, 0, 1)
+    glm.rotate(model, phi, 0, 1, 0)
+    cube['model'] = model
+
+
+# Build cube data
+V, I, O = makecube()
+vertices = V.view(gp.gloo.VertexBuffer)
+faces    = I.view(gp.gloo.IndexBuffer)
+outline  = O.view(gp.gloo.IndexBuffer)
+
+cube = gp.gloo.Program(vertex, fragment)
+cube.bind(vertices)
+transform = PVMProjection(Position3D("position"))
+cube['transform'] = transform
+window.attach(transform)
+
+phi, theta = 0, 0
+
+# OpenGL initalization
+gl.glClearColor(0.30, 0.30, 0.35, 1.00)
+gl.glEnable(gl.GL_DEPTH_TEST)
+gl.glPolygonOffset(1, 1)
+gl.glEnable(gl.GL_LINE_SMOOTH)
+gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
+gl.glLineWidth(0.75)
+
+# Run
+gp.run()
