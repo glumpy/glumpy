e00993733813a3404f1f61109faa217807764681
Nicolas Rougier <Nicolas.Rougier@inria.fr>
2014-08-02 20:08:53 +0200

    Added several arrow types

---
+++ examples/gloo-arrows.py
@@ -0,0 +1,349 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright (c) 2014, Nicolas P. Rougier. All Rights Reserved.
+# Distributed under the (new) BSD License.
+# -----------------------------------------------------------------------------
+import numpy as np
+from glumpy import app, gl, glm, gloo
+
+vertex = """
+    uniform vec2 iResolution;
+    attribute vec2 position;
+    attribute vec2 texcoord;
+    varying vec2 v_texcoord;
+
+    void main()
+    {
+        gl_Position = vec4(position, 0.0, 1.0);
+        v_texcoord = texcoord * iResolution;
+    }
+"""
+
+fragment = """
+vec4 stroke(float distance, float linewidth, float antialias, vec4 stroke)
+{
+    vec4 frag_color;
+    float t = linewidth/2.0 - antialias;
+    float signed_distance = distance;
+    float border_distance = abs(signed_distance) - t;
+    float alpha = border_distance/antialias;
+    alpha = exp(-alpha*alpha);
+
+    if( border_distance > (linewidth/2.0 + antialias) )
+        discard;
+    else if( border_distance < 0.0 )
+        frag_color = stroke;
+    else
+        frag_color = vec4(stroke.rgb, stroke.a * alpha);
+
+    return frag_color;
+}
+
+vec4 filled(float distance, float linewidth, float antialias, vec4 fill)
+{
+    vec4 frag_color;
+    float t = linewidth/2.0 - antialias;
+    float signed_distance = distance;
+    float border_distance = abs(signed_distance) - t;
+    float alpha = border_distance/antialias;
+    alpha = exp(-alpha*alpha);
+
+    // Within linestroke
+    if( border_distance < 0.0 )
+        frag_color = fill;
+    // Within shape
+    else if( signed_distance < 0.0 )
+        frag_color = fill;
+    else
+        // Outside shape
+        if( border_distance > (linewidth/2.0 + antialias) )
+            discard;
+        else // Line stroke exterior border
+            frag_color = vec4(fill.rgb, alpha * fill.a);
+
+    return frag_color;
+}
+
+// Computes the signed distance from a line
+float line_distance(vec2 p, vec2 p1, vec2 p2) {
+    vec2 center = (p1 + p2) * 0.5;
+    float len = length(p2 - p1);
+    vec2 dir = (p2 - p1) / len;
+    vec2 rel_p = p - center;
+    return dot(rel_p, vec2(dir.y, -dir.x));
+}
+
+// Computes the signed distance from a line segment
+float segment_distance(vec2 p, vec2 p1, vec2 p2) {
+    vec2 center = (p1 + p2) * 0.5;
+    float len = length(p2 - p1);
+    vec2 dir = (p2 - p1) / len;
+    vec2 rel_p = p - center;
+    float dist1 = abs(dot(rel_p, vec2(dir.y, -dir.x)));
+    float dist2 = abs(dot(rel_p, dir)) - 0.5*len;
+    return max(dist1, dist2);
+}
+
+vec4 circle_from_2_points(vec2 p1, vec2 p2, float radius)
+{
+    float q = length(p2-p1);
+    vec2 m = (p1+p2)/2.0;
+    vec2 d = vec2( sqrt(radius*radius - (q*q/4.0)) * (p1.y-p2.y)/q,
+                   sqrt(radius*radius - (q*q/4.0)) * (p2.x-p1.x)/q);
+    return  vec4(m+d, m-d);
+}
+
+float arrow_curved(vec2 texcoord,
+                   float body, float head,
+                   float linewidth, float antialias)
+{
+    float w = linewidth/2.0 + antialias;
+    vec2 start = -vec2(body/2.0, 0.0);
+    vec2 end   = +vec2(body/2.0, 0.0);
+    float height = 0.5;
+
+    vec2 p1 = end - head*vec2(+1.0,+height);
+    vec2 p2 = end - head*vec2(+1.0,-height);
+    vec2 p3 = end;
+
+    // Head : 3 circles
+    vec2 c1  = circle_from_2_points(p1, p3, 1.25*body).zw;
+    float d1 = length(texcoord - c1) - 1.25*body;
+    vec2 c2  = circle_from_2_points(p2, p3, 1.25*body).xy;
+    float d2 = length(texcoord - c2) - 1.25*body;
+    vec2 c3  = circle_from_2_points(p1, p2, max(body-head, 1.0*body)).xy;
+    float d3 = length(texcoord - c3) - max(body-head, 1.0*body);
+
+    // Body : 1 segment
+    float d4 = segment_distance(texcoord, start, end - vec2(linewidth,0.0));
+
+    // Outside (because of circles)
+    if( (texcoord.y - (1.5*height*head + antialias)) > 0.0 )
+         return 1000.0;
+    if( (texcoord.y + (1.5*height*head + antialias)) < 0.0 )
+         return 1000.0;
+    if( (texcoord.x + (body/2.0 + antialias)) < 0.0 )
+         return 1000.0;
+    if( (texcoord.x - (body + antialias)) > 0.0 )
+         return 1000.0;
+
+    return min( d4, -min(d3,min(d1,d2)));
+}
+
+float arrow_triangle(vec2 texcoord,
+                     float body, float head, float height,
+                     float linewidth, float antialias)
+{
+    float w = linewidth/2.0 + antialias;
+    vec2 start = -vec2(body/2.0, 0.0);
+    vec2 end   = +vec2(body/2.0, 0.0);
+
+    // Head : 3 lines
+    float d1 = line_distance(texcoord, end, end - head*vec2(+1.0,-height));
+    float d2 = line_distance(texcoord, end - head*vec2(+1.0,+height), end);
+    float d3 = texcoord.x - end.x + head;
+
+    // Body : 1 segment
+    float d4 = segment_distance(texcoord, start, end - vec2(linewidth,0.0));
+
+    float d = min(max(max(d1, d2), -d3), d4);
+    return d;
+}
+
+float arrow_triangle_90(vec2 texcoord,
+                        float body, float head,
+                        float linewidth, float antialias)
+{
+    return arrow_triangle(texcoord, body, head, 1.0, linewidth, antialias);
+}
+
+float arrow_triangle_60(vec2 texcoord,
+                        float body, float head,
+                        float linewidth, float antialias)
+{
+    return arrow_triangle(texcoord, body, head, 0.5, linewidth, antialias);
+}
+
+float arrow_triangle_30(vec2 texcoord,
+                        float body, float head,
+                        float linewidth, float antialias)
+{
+    return arrow_triangle(texcoord, body, head, 0.25, linewidth, antialias);
+}
+
+float arrow_angle(vec2 texcoord,
+                  float body, float head, float height,
+                  float linewidth, float antialias)
+{
+    float d;
+    float w = linewidth/2.0 + antialias;
+    vec2 start = -vec2(body/2.0, 0.0);
+    vec2 end   = +vec2(body/2.0, 0.0);
+
+    // Arrow tip (beyond segment end)
+    if( texcoord.x > body/2.0) {
+        // Head : 2 segments
+        float d1 = line_distance(texcoord, end, end - head*vec2(+1.0,-height));
+        float d2 = line_distance(texcoord, end - head*vec2(+1.0,+height), end);
+        // Body : 1 segment
+        float d3 = end.x - texcoord.x;
+        d = max(max(d1,d2), d3);
+    } else {
+        // Head : 2 segments
+        float d1 = segment_distance(texcoord, end - head*vec2(+1.0,-height), end);
+        float d2 = segment_distance(texcoord, end - head*vec2(+1.0,+height), end);
+        // Body : 1 segment
+        float d3 = segment_distance(texcoord, start, end - vec2(linewidth,0.0));
+        d = min(min(d1,d2), d3);
+    }
+    return d;
+}
+
+float arrow_angle_90(vec2 texcoord,
+                     float body, float head,
+                     float linewidth, float antialias)
+{
+    return arrow_angle(texcoord, body, head, 1.0, linewidth, antialias);
+}
+
+float arrow_angle_60(vec2 texcoord,
+                        float body, float head,
+                        float linewidth, float antialias)
+{
+    return arrow_angle(texcoord, body, head, 0.5, linewidth, antialias);
+}
+
+float arrow_angle_30(vec2 texcoord,
+                        float body, float head,
+                        float linewidth, float antialias)
+{
+    return arrow_angle(texcoord, body, head, 0.25, linewidth, antialias);
+}
+
+
+
+
+
+uniform vec2 iResolution;
+varying vec2 v_texcoord;
+void main()
+{
+    const float M_PI = 3.14159265358979323846;
+    float linewidth = 20.0;
+    float antialias =  1.0;
+
+    float body = 20.0*linewidth;
+    float head = 0.2 * body;
+    gl_FragColor = vec4(1,1,1,1);
+
+    float d = arrow_curved(v_texcoord, body, 0.25*body, linewidth, antialias);
+    // float d = arrow_triangle_90(v_texcoord, body, 0.15*body, linewidth, antialias);
+    // float d = arrow_triangle_60(v_texcoord, body, 0.20*body, linewidth, antialias);
+    // float d = arrow_triangle_30(v_texcoord, body, 0.25*body, linewidth, antialias);
+    // float d = arrow_angle_90(v_texcoord, body, 0.15*body, linewidth, antialias);
+    // float d = arrow_angle_60(v_texcoord, body, 0.20*body, linewidth, antialias);
+    // float d = arrow_angle_30(v_texcoord, body, 0.25*body, linewidth, antialias);
+    gl_FragColor = filled(d, linewidth, antialias, vec4(0,0,0,1));
+
+
+/*
+    // Latex
+    float height = 0.5;
+    vec2 p1 = end - head_length*vec2(+1.0,+height);
+    vec2 p2 = end - head_length*vec2(+1.0,-height);
+    vec2 p3 = end;
+    vec2 c1  = circle_from_2_points(p1, p3, 1.25*length).zw;
+    float d1 = length(v_texcoord - c1) - 1.25*length;
+    vec2 c2  = circle_from_2_points(p2, p3, 1.25*length).xy;
+    float d2 = length(v_texcoord - c2) - 1.25*length;
+    vec2 c3  = circle_from_2_points(p1, p2, max(length-head_length, 1.0*length)).xy;
+    float d3 = length(v_texcoord - c3) - max(length-head_length, 1.0*length);
+    float d4 = segment_distance(v_texcoord, start, end - vec2(linewidth,0.0));
+    float d = min(d4, -min(d3,min(d1,d2)));
+
+    // d = max(d,v_texcoord.y+linewidth/2.0);
+    gl_FragColor = filled(d, linewidth, antialias, vec4(0,0,0,1));
+*/
+
+/*
+    // Angle 60
+    float r = .5;
+    float d;
+    if( v_texcoord.x > length/2.0) {
+        float d1 = line_distance(v_texcoord, end, end - head_length*vec2(+1.0,-r));
+        float d2 = line_distance(v_texcoord, end - head_length*vec2(+1.0,+r), end);
+        float d3 = end.x - v_texcoord.x;
+        d = max(max(d1,d2), d3);
+    } else {
+        float d1 = segment_distance(v_texcoord, end - head_length*vec2(+1.0,-r), end);
+        float d2 = segment_distance(v_texcoord, end - head_length*vec2(+1.0,+r), end);
+        float d3 = segment_distance(v_texcoord, start, end - vec2(linewidth,0.0));
+        d = min(min(d1,d2), d3);
+    }
+    gl_FragColor = filled(d, linewidth, antialias, vec4(0,0,0,1));
+*/
+
+/*
+    // Latex
+    float d1 = line_distance(v_texcoord, end-head_length*vec2(+1.0,-0.5), end);
+    float d2 = line_distance(v_texcoord, end-head_length*vec2(+1.0,-0.5), end-vec2(3.*head_length/4.,0.0));
+    float d3 = line_distance(v_texcoord, end-head_length*vec2(+1.0,+0.5), end);
+    float d4 = line_distance(v_texcoord, end-head_length*vec2(+1.0,+0.5), end-vec2(3.*head_length/4.,0.0));
+    float d5 = segment_distance(v_texcoord, start, end - vec2(linewidth,0.0));
+    float d = min(d5, max( max(-d1, d3), - max(-d2,d4)));
+    gl_FragColor = filled(d, linewidth, antialias, vec4(0,0,0,1));
+*/
+
+/*
+    // Angle 90
+    float d1 = segment_distance(v_texcoord, end - head_length*vec2(+1.0,-1.0), end);
+    float d2 = segment_distance(v_texcoord, end - head_length*vec2(+1.0,+1.0), end);
+    float d3 = segment_distance(v_texcoord, start, end - vec2(linewidth,0.0));
+    float d = min(min(d1, d2), d3);
+    gl_FragColor = filled(d, linewidth, antialias, vec4(0,0,0,1));
+*/
+
+/*
+    // Triangle 90
+    float d1 = line_distance(v_texcoord, end, end - head_length*vec2(+1.0,-1.0));
+    float d2 = line_distance(v_texcoord, end - head_length*vec2(+1.0,+1.0), end);
+    float d3 = v_texcoord.x - end.x + head_length;
+    float d4 = segment_distance(v_texcoord, start, end - vec2(linewidth,0.0));
+    float d = min(max(max(d1, d2), -d3), d4);
+    gl_FragColor = filled(d, linewidth, antialias, vec4(0,0,0,1));
+*/
+
+/*
+    // Triangle 45
+    float d1 = line_distance(v_texcoord, end, end - head_length*vec2(+1.0,-0.5));
+    float d2 = line_distance(v_texcoord, end - head_length*vec2(+1.0,+0.5), end);
+    float d3 = v_texcoord.x - end.x + head_length;
+    float d4 = segment_distance(v_texcoord, start, end - vec2(linewidth,0.0));
+    float d = min(max(max(d1, d2), -d3), d4);
+    gl_FragColor = filled(d, linewidth, antialias, vec4(0,0,0,1));
+*/
+}
+"""
+
+
+window = app.Window(width=800, height=800)
+
+@window.event
+def on_draw():
+    gl.glClear(gl.GL_COLOR_BUFFER_BIT)
+    program.draw(gl.GL_TRIANGLE_STRIP)
+
+@window.event
+def on_resize(width, height):
+    program["iResolution"] = width, height
+
+program = gloo.Program(vertex, fragment, count=4)
+
+dx,dy = 1,.5
+program['position'] = (-dx,-dy), (-dx,+dy), (+dx,-dy), (+dx,+dy)
+program['texcoord'] = (-.5*dx,-.5*dy), (-.5*dx,+.5*dy), (+.5*dx,-.5*dy), (+.5*dx,+.5*dy)
+gl.glClearColor(1,1,1,1)
+gl.glEnable(gl.GL_BLEND)
+gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
+app.run()
