cbaa7fb8bc058798202dc5d8619b88bf77a9955b
Nicolas Rougier <Nicolas.Rougier@inria.fr>
2014-08-03 08:37:13 +0200

    Cosmetic fix

--- examples/gloo-quiver.py
+++ examples/gloo-quiver.py
@@ -249,25 +249,25 @@ void main()
 {
     const float M_PI = 3.14159265358979323846;
     const float SQRT_2 = 1.4142135623730951;
-
-    const float rows = 12.0;
-    const float cols = 12.0;
-    const float linewidth = 1.0;
+    const float linewidth = 2.0;
     const float antialias =  1.0;
+
+    const float rows = 16.0;
+    const float cols = 16.0;
     float body = min(iResolution.x/cols, iResolution.y/rows) / SQRT_2;
     vec2 texcoord = gl_FragCoord.xy;
     vec2 size   = iResolution.xy / vec2(cols,rows);
     vec2 center = (floor(texcoord/size) + vec2(0.5,0.5)) * size;
     texcoord -= center;
 
-    float theta = M_PI/3.0;
+    float theta = M_PI/3.0 + 0.05*(center.x / cols + center.y / rows);
     float cos_theta = cos(theta);
     float sin_theta = sin(theta);
     texcoord = vec2(cos_theta*texcoord.x - sin_theta*texcoord.y,
                     sin_theta*texcoord.x + cos_theta*texcoord.y);
 
-    float d = arrow_curved(texcoord, body, 0.25*body, linewidth, antialias);
-    // float d = arrow_stealth(texcoord, body, 0.25*body, linewidth, antialias);
+    // float d = arrow_curved(texcoord, body, 0.25*body, linewidth, antialias);
+    float d = arrow_stealth(texcoord, body, 0.25*body, linewidth, antialias);
     // float d = arrow_triangle_90(texcoord, body, 0.15*body, linewidth, antialias);
     // float d = arrow_triangle_60(texcoord, body, 0.20*body, linewidth, antialias);
     // float d = arrow_triangle_30(texcoord, body, 0.25*body, linewidth, antialias);
