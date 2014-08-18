d51c15adb289055c818c6cc1fb1e3a14e1affd23
Nicolas Rougier <Nicolas.Rougier@inria.fr>
2014-08-18 15:44:00 +0200

    Simplified code

--- examples/gloo-rain.py
+++ examples/gloo-rain.py
@@ -4,21 +4,12 @@
 # Copyright (c) 2014, Nicolas P. Rougier. All Rights Reserved.
 # Distributed under the (new) BSD License.
 # -----------------------------------------------------------------------------
-import sys
 import numpy as np
-
-import glumpy
-import glumpy.gl as gl
-import glumpy.app as app
-import glumpy.glm as glm
-import glumpy.gloo as gloo
-
+from glumpy import app, gloo, gl, glm
 
 vertex = """
 #version 120
 
-uniform mat4  u_model;
-uniform mat4  u_view;
 uniform mat4  u_projection;
 uniform float u_linewidth;
 uniform float u_antialias;
@@ -36,12 +27,12 @@ void main (void)
     v_fg_color = a_fg_color;
     if( a_fg_color.a > 0.0)
     {
-        gl_Position = u_projection * u_view * u_model * vec4(a_position,1.0);
+        gl_Position = u_projection * vec4(a_position,1.0);
         gl_PointSize = v_size + u_linewidth + 2*1.5*u_antialias;
     }
     else
     {
-        gl_Position = u_projection * u_view * u_model * vec4(-1,-1,0,1);
+        gl_Position = u_projection * vec4(-1,-1,0,1);
         gl_PointSize = 0.0;
     }
 }
@@ -92,9 +83,7 @@ def on_draw(dt):
 
 @window.event
 def on_resize(width, height):
-    gl.glViewport(0, 0, width, height)
-    projection = glm.ortho(0, width, 0, height, -1, +1)
-    program['u_projection'] = projection
+    program['u_projection'] = glm.ortho(0, width, 0, height, -1, +1)
 
 @window.timer(1/60.)
 def timer(fps):
@@ -115,12 +104,9 @@ dtype =  [('a_position', np.float32, 2),
           ('a_growth',   np.float32, 1)]
 data = np.zeros(120,dtype).view(gloo.VertexBuffer)
 
-
 index = 0
 program = gloo.Program(vertex, fragment)
 program.bind(data)
 program['u_antialias'] = 1.00
 program['u_linewidth'] = 1.00
-program['u_model'] = np.eye(4, dtype=np.float32)
-program['u_view'] = np.eye(4, dtype=np.float32)
-app.run(framerate=60)
+app.run()
