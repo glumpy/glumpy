3f8500c0c091c5ec715966782df695fe5afce189
Nicolas Rougier <Nicolas.Rougier@inria.fr>
2014-08-22 08:46:43 +0200

    Simplification

--- examples/app-two-windows.py
+++ examples/app-two-windows.py
@@ -4,19 +4,15 @@
 # Copyright (c) 2014, Nicolas P. Rougier
 # Distributed under the (new) BSD License. See LICENSE.txt for more info.
 # -----------------------------------------------------------------------------
-from glumpy import gl, app
+from glumpy import app
 
-window1 = app.Window()
-window2 = app.Window()
+window1 = app.Window(color=(0,0,0,1))
+window2 = app.Window(color=(1,1,1,1))
 
 @window1.event
-def on_draw(dt):
-    gl.glClearColor(0,0,0,1)
-    window1.clear()
+def on_draw(dt): window1.clear()
 
 @window2.event
-def on_draw(dt):
-    gl.glClearColor(1,1,1,1)
-    window2.clear()
+def on_draw(dt): window2.clear()
 
 app.run()
