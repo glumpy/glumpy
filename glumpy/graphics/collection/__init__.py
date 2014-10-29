c8068a60d4e0a2e4f3f272f5db19ced24fdd9b2a
Nicolas Rougier <Nicolas.Rougier@inria.fr>
2014-10-29 20:45:06 +0100

    Fix wrong import in collection

--- glumpy/graphics/collection/__init__.py
+++ glumpy/graphics/collection/__init__.py
@@ -4,16 +4,10 @@
 # Distributed under the terms of the new BSD License.
 # -----------------------------------------------------------------------------
 from . base_collection import BaseCollection
-
 from . glyph_collection import GlyphCollection
-
 from . path_collection import PathCollection
-
 from . point_collection import PointCollection
-
-from . agg_path_collection import AggPathCollection
-
-from . marker_collection import MarkerCollection
+# from . agg_path_collection import AggPathCollection
+# from . marker_collection import MarkerCollection
 from . unimarker_collection import UnimarkerCollection
-
 from . agg_solid_segment_collection import AggSolidSegmentCollection
