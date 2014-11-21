5306e15735a40270674905c1a5db387fe41ff8b3
Nicolas Rougier <Nicolas.Rougier@inria.fr>
2014-11-21 10:53:01 +0100

    Fix issue #6 with ffmpeg dependency

--- glumpy/ext/__init__.py
+++ glumpy/ext/__init__.py
@@ -3,11 +3,17 @@
 # Copyright (c) 2014, Nicolas P. Rougier
 # Distributed under the (new) BSD License. See LICENSE.txt for more info.
 # -----------------------------------------------------------------------------
+from . import glfw
+from . import freetype
+
 try:
     from . import sdf
 except:
     sdf = None
-from . import glfw
-from . import freetype
-from . import ffmpeg_reader
-from . import ffmpeg_writer
+
+try:
+    from . import ffmpeg_reader
+    from . import ffmpeg_writer
+except:
+    ffmpeg_reader = None
+    ffmpeg_writer = None
