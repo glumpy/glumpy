39751c3531eb3dce286ca232033b1c4f92f57df7
Nicolas Rougier <Nicolas.Rougier@inria.fr>
2014-11-23 12:32:38 +0100

    Surround error on copy with try/except (see issue #5)

--- glumpy/gl.py
+++ glumpy/gl.py
@@ -3,16 +3,22 @@
 # Copyright (c) 2014, Nicolas P. Rougier
 # Distributed under the (new) BSD License. See LICENSE.txt for more info.
 # -----------------------------------------------------------------------------
+from glumpy.log import log
 from OpenGL.arrays import numpymodule
-numpymodule.NumpyHandler.ERROR_ON_COPY = True
+
+try:
+    numpymodule.NumpyHandler.ERROR_ON_COPY = True
+except TypeError:
+    log.warn("Cannot set error on copy on GPU copy")
+
 
 from OpenGL import contextdata
 def cleanupCallback( context=None ):
-	"""Create a cleanup callback to clear context-specific storage for the current context"""
-	def callback( context = contextdata.getContext( context ) ):
-		"""Clean up the context, assumes that the context will *not* render again!"""
-		contextdata.cleanupContext( context )
-	return callback
+    """Create a cleanup callback to clear context-specific storage for the current context"""
+    def callback( context = contextdata.getContext( context ) ):
+        """Clean up the context, assumes that the context will *not* render again!"""
+        contextdata.cleanupContext( context )
+    return callback
 
 from OpenGL.GL import *
 from OpenGL.GL.EXT.geometry_shader4 import *
