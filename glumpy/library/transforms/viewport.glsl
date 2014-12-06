734508bba6c651b58075451a7f5d28d52096c85a
Nicolas Rougier <Nicolas.Rougier@inria.fr>
2014-12-06 08:50:31 +0100

    Added missing shaders & transforms

---
+++ glumpy/library/transforms/viewport.glsl
@@ -0,0 +1,6 @@
+// ----------------------------------------------------------------------------
+// Copyright (c) 2014, Nicolas P. Rougier. All Rights Reserved.
+// Distributed under the (new) BSD License.
+// ----------------------------------------------------------------------------
+
+uniform vec4 viewport;
---
+++ glumpy/transforms/viewport.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright (c) 2014, Nicolas P. Rougier
+# Distributed under the (new) BSD License. See LICENSE.txt for more info.
+# -----------------------------------------------------------------------------
+from glumpy import library
+from . transform import Transform
+
+class Viewport(Transform):
+
+    def __init__(self, *args, **kwargs):
+        code = library.get("transforms/viewport.glsl")
+        Transform.__init__(self, code, *args, **kwargs)
+
+    def on_resize(self, width, height):
+        self["viewport"] = 0, 0, width, height
