cba7c10259253068dd5ea768ef7b636cc23e16b4
Nicolas Rougier <Nicolas.Rougier@inria.fr>
2014-12-16 08:31:26 +0100

    Fix example

--- glumpy/graphics/collection/agg_segment_collection.py
+++ glumpy/graphics/collection/agg_segment_collection.py
@@ -33,25 +33,38 @@ class AggSegmentCollection(Collection):
 
 
     def append(self, P0, P1, **kwargs):
+        """ """
 
-        V,I = self.bake(P0, P1)
+        # WARNING: we must take care of kwargs when they relates to an attribute
+        count = len(P0)
+        V = np.zeros(count, dtype=self.vtype)
         U = np.zeros(len(P0), dtype=self.utype) if self.utype else None
         protect = ["P0", "P1", "index"]
         self.apply_defaults(V, U, protect=protect, **kwargs)
-        Collection.append(self, vertices=V, uniforms=U, indices=I)
-
-
-    def bake(self, P0, P1):
-
-        count = len(P0)
-        V = np.zeros(count, dtype=self.vtype)
-
         V['P0'] = P0
         V['P1'] = P1
         V = V.repeat(4,axis=0)
-
         V['index'] = np.resize([0,1,2,3], 4*count)
         I = np.resize( np.array([0,1,2,0,2,3], dtype=np.uint32), 6*count)
         I += np.repeat( 4*np.arange(count), 6)
+        Collection.append(self, vertices=V, uniforms=U, indices=I)
 
-        return V, I
+
+    # def append(self, P0, P1, **kwargs):
+    #     V,I = self.bake(P0, P1)
+    #     U = np.zeros(len(P0), dtype=self.utype) if self.utype else None
+    #     protect = ["P0", "P1", "index"]
+    #     self.apply_defaults(V, U, protect=protect, **kwargs)
+    #     Collection.append(self, vertices=V, uniforms=U, indices=I)
+
+
+    # def bake(self, P0, P1):
+    #     count = len(P0)
+    #     V = np.zeros(count, dtype=self.vtype)
+    #     V['P0'] = P0
+    #     V['P1'] = P1
+    #     V = V.repeat(4,axis=0)
+    #     V['index'] = np.resize([0,1,2,3], 4*count)
+    #     I = np.resize( np.array([0,1,2,0,2,3], dtype=np.uint32), 6*count)
+    #     I += np.repeat( 4*np.arange(count), 6)
+    #     return V, I
