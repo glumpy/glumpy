aa723f1f37c92739e25c54e1d6a853500b4ef687
Nicolas Rougier <Nicolas.Rougier@inria.fr>
2015-01-04 19:46:54 +0100

    Bug fix

--- glumpy/transforms/__init__.py
+++ glumpy/transforms/__init__.py
@@ -3,7 +3,7 @@
 # Copyright (c) 2014, Nicolas P. Rougier. All rights reserved.
 # Distributed under the terms of the new BSD License.
 # -----------------------------------------------------------------------------
-from rotate import Rotate
+# from rotate import Rotate
 from panzoom import PanZoom
 from viewport import Viewport
 from trackball import Trackball
@@ -13,7 +13,7 @@ from position2d import Position2D
 from position3d import Position3D
 from polar import PolarProjection
 from pvm_projection import PVMProjection
-from perpective_projection import PerspectiveProjection
+# from perpective_projection import PerspectiveProjection
 from orthographic_projection import OrthographicProjection
 
 from log_scale import LogScale
