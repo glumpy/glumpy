b2d602ba1c5fef93e91363c9793cb0b1fb3a3e68
Nicolas Rougier <Nicolas.Rougier@inria.fr>
2015-01-09 07:04:07 +0100

    Move clipping to viewport

--- glumpy/library/transforms/viewport.glsl
+++ glumpy/library/transforms/viewport.glsl
@@ -2,5 +2,32 @@
 // Copyright (c) 2014, Nicolas P. Rougier. All Rights Reserved.
 // Distributed under the (new) BSD License.
 // ----------------------------------------------------------------------------
+uniform vec4 local_viewport;
+uniform vec4 global_viewport;
 
-uniform vec4 viewport;
+#ifdef __VERTEX_SHADER__
+void transform(void)
+{
+    vec4 position = gl_Position;
+
+    float w = local_viewport.z / global_viewport.z;
+    float h = local_viewport.w / global_viewport.w;
+    float x = 2.0*(local_viewport.x / global_viewport.z) - 1.0 + w;
+    float y = 2.0*(local_viewport.y / global_viewport.w) - 1.0 + h;
+
+    gl_Position = vec4((x + w*position.x/position.w)*position.w,
+                       (y + h*position.y/position.w)*position.w,
+                       position.z, position.w);
+}
+#endif
+
+#ifdef __FRAGMENT_SHADER__
+void clipping(void)
+{
+    vec2 position = gl_FragCoord.xy;
+         if( position.x < (local_viewport.x))                  discard;
+    else if( position.x > (local_viewport.x+local_viewport.z)) discard;
+    else if( position.y < (local_viewport.y))                  discard;
+    else if( position.y > (local_viewport.y+local_viewport.w)) discard;
+}
+#endif
