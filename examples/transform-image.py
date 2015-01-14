093a2efa9f980e513a51e7f9abaf8e1632e38618
Nicolas Rougier <Nicolas.Rougier@inria.fr>
2015-01-14 15:20:28 +0100

    Added mouse zoom

--- examples/transform-image.py
+++ examples/transform-image.py
@@ -39,13 +39,14 @@ def on_draw(dt):
 program = gloo.Program(vertex, fragment, count=4)
 program['position'] = [(-1,-1), (-1,+1), (+1,-1), (+1,+1)]
 program['texture'] = data.get("earth.jpg")
+program['texture'].interpolation = gl.GL_LINEAR
 
 
 # Polar projection
 # program['projection'] = PolarProjection(
 #     # This translates texture coordinates to cartesian coordinates
-#     LinearScale('.x', name = 'x', domain=(-1, 1), range=(-1,1), call="forward"),
-#     LinearScale('.y', name = 'y', domain=(-1, 1), range=(-1,1), call="forward"))
+#     LinearScale('.x', name = 'x', domain=(-1, 1), range=(-1,1), call="forward", clamp=True),
+#     LinearScale('.y', name = 'y', domain=(-1, 1), range=(-1,1), call="forward", clamp=True))
 # program['scale'] = Position(
 #     # This translates cartesian coordinates (polar domains) to texture coordinates
 #     # LogScale('.x', name = 'x', domain=(-1,0), range=(0,1), clamp=True),
@@ -55,11 +56,11 @@ program['texture'] = data.get("earth.jpg")
 # Hammer projection
 # program['projection'] = HammerProjection(
 #     # This translates texture coordinates to cartesian coordinates
-#     LinearScale('.x', name = 'x', domain=(-1, 1), range=(-3.0,3.0), call="forward"),
-#     LinearScale('.y', name = 'y', domain=(+1,-1), range=(-2.0,2.0), call="forward"))
+#     LinearScale('.x', name = 'x', domain=(-1, 1), range=(-3.0,3.0), call="forward", clamp=True),
+#     LinearScale('.y', name = 'y', domain=(+1,-1), range=(-2.0,2.0), call="forward", clamp=True))
 
 # program['scale'] = Position(
-#     # This translates cartesian coordinates (polar domains) to texture coordinates
+#     # This translates cartesian coordinates (hammer domains) to texture coordinates
 #     LinearScale('.x', name = 'x', domain=(-np.pi,   np.pi),   range=(0,1), clamp=True),
 #     LinearScale('.y', name = 'y', domain=(-np.pi/2, np.pi/2), range=(0,1), clamp=True))
 # window.set_size(800,500)
@@ -67,13 +68,28 @@ program['texture'] = data.get("earth.jpg")
 # Transverse Mercator projection
 program['projection'] = TransverseMercatorProjection(
     # This translates texture coordinates to cartesian coordinates
-    LinearScale('.x', name = 'x', domain=(-1,+1), range=(-1.5,1.5), call="forward"),
-    LinearScale('.y', name = 'y', domain=(+1,-1), range=(-2.4,2.4), call="forward"))
+    LinearScale('.x', name='x', domain=(-1,+1), range=(-1.5,1.5), call="forward", clamp=True),
+    LinearScale('.y', name='y', domain=(+1,-1), range=(-2.4,2.4), call="forward", clamp=True))
 
 program['scale'] = Position(
-    # This translates cartesian coordinates (polar domains) to texture coordinates
+    # This translates cartesian coordinates (mercator domains) to texture coordinates
     LinearScale('.x', name = 'x', domain=(-np.pi,   np.pi),   range=(0,1), clamp=True),
     LinearScale('.y', name = 'y', domain=(-np.pi/2, np.pi/2), range=(0,1), clamp=True))
 window.set_size(500,800)
+
+
+@window.event
+def on_mouse_scroll(x, y, dx, dy):
+    xdomain = program['projection']['x']['domain']
+    ydomain = program['projection']['y']['domain']
+    if dy > 0:
+        xdomain *= .9
+        ydomain *= .9
+    elif dy < 0:
+        xdomain *= 1.1
+        ydomain *= 1.1
+    program['projection']['x']['domain'] = xdomain
+    program['projection']['y']['domain'] = ydomain
+
 
 app.run()
