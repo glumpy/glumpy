6d156bf8142603f3447d8434a5da91219a74578a
Nicolas Rougier <Nicolas.Rougier@inria.fr>
2015-05-31 20:26:12 +0200

    Modified height

--- examples/spherical-harmonics.py
+++ examples/spherical-harmonics.py
@@ -55,13 +55,13 @@ void main()
     v_phi = phi;
     v_theta = theta;
 
-    radius = .5 + (harmonic(theta,phi,m1))/8.0;
+    radius = 0.9 + 0.2*(harmonic(theta,phi,m1));
     x = sin(theta) * sin(phi) * radius;
     y = sin(theta) * cos(phi) * radius;
     z = cos(theta) * radius;
     vec3 position1 = vec3(x,y,z);
 
-    radius = .5 + (harmonic(theta,phi,m2))/8.0;
+    radius = 0.9 + 0.2*(harmonic(theta,phi,m2));
     x = sin(theta) * sin(phi) * radius;
     y = sin(theta) * cos(phi) * radius;
     z = cos(theta) * radius;
