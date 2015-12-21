74221c2c3b1cdc9b7c571733d57bb03e0477ffa4
Nicolas Rougier <Nicolas.Rougier@inria.fr>
2015-12-21 10:03:58 +0100

    Added import of several base Objects (for documentation)

--- glumpy/gloo/__init__.py
+++ glumpy/gloo/__init__.py
@@ -1,16 +1,19 @@
-# -*- coding: utf-8 -*-
 # -----------------------------------------------------------------------------
-# Copyright (c) 2014, Nicolas P. Rougier. All rights reserved.
+# Copyright (c) 2011-2016, Nicolas P. Rougier. All rights reserved.
 # Distributed under the terms of the new BSD License.
 # -----------------------------------------------------------------------------
 from . atlas import Atlas
 from . snippet import Snippet
 from . program import Program
+from . gpudata import GPUData
+from . globject import GLObject
 from . uniforms import Uniforms
+from . texture import Texture
 from . texture import TextureCube
 from . texture import Texture1D, Texture2D
 from . texture import TextureFloat1D, TextureFloat2D
 from . texture import DepthTexture
+from . buffer import Buffer
 from . buffer import VertexBuffer, IndexBuffer
 from . shader import VertexShader, FragmentShader, GeometryShader
 from . framebuffer import FrameBuffer, ColorBuffer, DepthBuffer, StencilBuffer
