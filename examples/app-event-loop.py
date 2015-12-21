2b0c09ce1468fd124b69225fcaeb68562bf168b0
Nicolas Rougier <Nicolas.Rougier@inria.fr>
2015-12-21 09:17:46 +0100

    Updated copyright notice and removed shebang

--- examples/app-event-loop.py
+++ examples/app-event-loop.py
@@ -1,7 +1,5 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
 # -----------------------------------------------------------------------------
-# Copyright (c) 2014, Nicolas P. Rougier
+# Copyright (c) 2011-2016, Nicolas P. Rougier
 # Distributed under the (new) BSD License. See LICENSE.txt for more info.
 # -----------------------------------------------------------------------------
 """ This example shows how to run the event loop manually """
