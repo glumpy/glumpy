3e7ac0ae6dac8141bd5c795662bf4eda54553795
Martin Lizee <gmartinlizee@gmail.com>
2018-12-01 12:00:46 -0500

    Working version with rotation example

--- examples/pyimgui/gui-quad-rotation.py
+++ examples/pyimgui/gui-quad-rotation.py
@@ -3,7 +3,8 @@
 # Distributed under the (new) BSD License.
 # -----------------------------------------------------------------------------
 from glumpy import app, gloo, gl
-import imgui
+from glumpy.transforms import PanZoom, Position
+
 app.use("pyimgui")
 
 vertex = """
@@ -17,7 +18,8 @@ vertex = """
         float st = sin(theta);
         float x = 0.75* (position.x*ct - position.y*st);
         float y = 0.75* (position.x*st + position.y*ct);
-        gl_Position = vec4(x, y, 0.0, 1.0);
+        vec4 pos =  (vec4(x, y, 0.0, 1.0));
+        gl_Position = <transform>;
         v_color = color;
     } """
 
@@ -38,38 +40,31 @@ quad['position'] = [ (-1,-1),   (-1,+1),   (+1,-1),   (+1,+1)   ]
 quad['theta'] = 0
 
 # Create a window with a valid GL context
-window = app.Window()
+window = app.Window(width=1024, height=768, color=(.1,.1,.4,1))
+
+transform = PanZoom(Position("pos"),aspect=1)
+quad['transform'] = transform
+window.attach(transform)
 
 theta = 0.0
+speed = 1.0
+
+# Tell glumpy what needs to be done at each redraw
+@window.event
+def on_gui(dt):
+    gui = window.gui
+    gui.begin("Custom window", True)
+    gui.text("Bar")
+    gui.text_colored("Eggs", 0.2, 1., 0.)
+    gui.end()
 
 # Tell glumpy what needs to be done at each redraw
 @window.event
 def on_draw(dt):
-    imgui.new_frame()
-    if imgui.begin_main_menu_bar():
-        if imgui.begin_menu("File", True):
-
-            clicked_quit, selected_quit = imgui.menu_item(
-                "Quit", 'Cmd+Q', False, True
-            )
-
-            if clicked_quit:
-                exit(1)
-
-            imgui.end_menu()
-        imgui.end_main_menu_bar()
-
-    imgui.show_test_window()
-
-    imgui.begin("Custom window", True)
-    imgui.text("Bar")
-    imgui.text_colored("Eggs", 0.2, 1., 0.)
-    imgui.end()
-
     window.clear()
-    quad['theta'] += dt
+    quad['theta'] += dt*speed
     quad.draw(gl.GL_TRIANGLE_STRIP)
-    window.render_gui()
+
 
 # Run the app
 app.run()
