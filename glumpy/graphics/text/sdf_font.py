264cb30c55fcbf49859b4cf84f5375e965dd3ab0
QiuJiangkun <qiujiangkun@foxmail.com>
2020-11-26 00:02:18 +0800

    BUGFIX: https://github.com/glumpy/glumpy/issues/268

--- glumpy/graphics/text/sdf_font.py
+++ glumpy/graphics/text/sdf_font.py
@@ -42,8 +42,8 @@ def zoom(Z, ratio):
     """ Bilinear image zoom """
 
     nrows, ncols = Z.shape
-    x,y = np.meshgrid(np.linspace(0, ncols, (ratio*ncols), endpoint=False),
-                      np.linspace(0, nrows, (ratio*nrows), endpoint=False))
+    x,y = np.meshgrid(np.linspace(0, ncols, round(ratio*ncols), endpoint=False),
+                      np.linspace(0, nrows, round(ratio*nrows), endpoint=False))
     return bilinear_interpolate(Z, x, y)
 
 
